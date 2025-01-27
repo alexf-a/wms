f5dc8faceece749cca35730ade52c0b27a61798e
alexf-a <alexfa0845@gmail.com>
2025-01-26 19:21:02 -0800

    Add Bin model with fields for name, description, QR code, and dimensions

--- core/models.py
+++ core/models.py
@@ -1,6 +1,19 @@
 from django.contrib.auth.models import User
+from django.db import models
 
 class WMSUser(User):
     class Meta:
         app_label = 'core'
 # Create your models here.
+
+class Bin(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    qr_code = models.ImageField(upload_to='qr_codes/')
+    location = models.CharField(max_length=255, blank=True, null=True)
+    length = models.FloatField(blank=True, null=True)
+    width = models.FloatField(blank=True, null=True)
+    height = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
