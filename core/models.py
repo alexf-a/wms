bd7fb836ceab2c9aa2210261e4b338d98f838c8d
alexf-a <alexfa0845@gmail.com>
2025-02-01 19:47:51 -0800

    Make Bin name unique and auto-generate QR code on save

--- core/models.py
+++ core/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
+import qrcode
+from django.core.files.base import ContentFile
+from io import BytesIO
 
 class WMSUser(User):
     class Meta:
@@ -7,13 +10,22 @@ class WMSUser(User):
 # Create your models here.
 
 class Bin(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)  # Make name unique
     description = models.TextField()
     qr_code = models.ImageField(upload_to='qr_codes/')
     location = models.CharField(max_length=255, blank=True, null=True)
     length = models.FloatField(blank=True, null=True)
     width = models.FloatField(blank=True, null=True)
     height = models.FloatField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.qr_code:
+            qr = qrcode.make(self.name)
+            buffer = BytesIO()
+            qr.save(buffer, format='PNG')
+            file_name = f'{self.name}_qr.png'
+            self.qr_code.save(file_name, ContentFile(buffer.getvalue()), save=False)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
