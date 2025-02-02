6502b5f59143d4b3d0cc1ec178dbe198eb0015ce
alexf-a <alexfa0845@gmail.com>
2025-02-01 18:40:11 -0800

    Add create_bin_view to handle bin creation with file upload and attributes

--- core/views.py
+++ core/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from .forms import WMSUserCreationForm
-from .models import WMSUser
+from .models import WMSUser, Bin
+from django.core.files.storage import FileSystemStorage
 
 def register_view(request):
     if request.method == 'POST':
@@ -39,6 +40,30 @@ def expand_inventory_view(request):
     return render(request, 'core/expand_inventory.html')
 
 def create_bin_view(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        description = request.POST['description']
+        qr_code = request.FILES['qr_code']
+        location = request.POST.get('location', '')
+        length = request.POST.get('length', None)
+        width = request.POST.get('width', None)
+        height = request.POST.get('height', None)
+
+        fs = FileSystemStorage()
+        filename = fs.save(qr_code.name, qr_code)
+        qr_code_url = fs.url(filename)
+
+        bin = Bin(
+            name=name,
+            description=description,
+            qr_code=qr_code_url,
+            location=location,
+            length=length,
+            width=width,
+            height=height
+        )
+        bin.save()
+        return redirect('inventory_view')
     return render(request, 'core/create_bin.html')
 
 def add_items_to_bin_view(request):
