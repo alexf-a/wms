ffc0b0db175055b6c99dd6adf66f2b2a2dd0536c
alexf-a <alexfa0845@gmail.com>
2025-02-15 16:06:06 -0800

    Add ItemForm for creating and managing Item instances in the application

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
-from .models import WMSUser
+from django import forms
+from .models import WMSUser, Item, Bin
 
 class WMSUserCreationForm(UserCreationForm):
     class Meta:
         model = WMSUser
         fields = ('username', 'password1', 'password2')
+
+class ItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ['name', 'description', 'image', 'bin']
