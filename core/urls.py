211f50dd1806a4102ebc7f5d57c68cba26bc37f3
alexf-a <alexfa0845@gmail.com>
2025-02-15 18:52:44 -0800

    Add list_bins URL route and update login view template path

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import list_bins
 
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('register/', views.register_view, name='register'),
-    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True, next_page='inventory_view'), name='login'),
+    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True, template_name='core/auth/login.html', next_page='inventory_view'), name='login'),
     path('inventory/', views.inventory_view, name='inventory_view'),
     path('expand_inventory/', views.expand_inventory_view, name='expand_inventory'),
     path('create_bin/', views.create_bin_view, name='create_bin'),
-    path('add_items_to_bin/', views.add_items_to_bin_view, name='add_items_to_bin')
+    path('add_items_to_bin/', views.add_items_to_bin_view, name='add_items_to_bin'),
+    path('bins/', list_bins, name='list_bins'),
 ]
