01f0ff37ad2799915631a647259535c3bf8de2e0
alexf-a <alexfa0845@gmail.com>
2025-03-29 21:26:46 -0800

    Refactor ModelID class to use combined metaclass EnumABCMeta for improved compatibility

--- llm/model_id.py
+++ llm/model_id.py
@@ -1,10 +1,10 @@
 from abc import ABC, ABCMeta
-from enum import Enum
-
+from enum import Enum, EnumMeta
+from typing import Any, ClassVar
 
 # Create a compatible metaclass that inherits from both EnumMeta and ABCMeta
-class EnumABCMeta(type(Enum), ABCMeta):
+class EnumABCMeta(EnumMeta, ABCMeta):
     """A metaclass that combines EnumMeta and ABCMeta."""
 
-class ModelID(Enum, ABC):
+class ModelID(ABC, Enum, metaclass=EnumABCMeta):
     """Enumeration of available models."""
