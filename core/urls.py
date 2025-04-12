3a0c50b3d243f22edde00ab5ee6535c78f44ed18
alexf-a <alexfa0845@gmail.com>
2025-04-12 10:47:20 -0700

    Refactor home and inventory views to enhance authentication handling and update redirect paths

--- core/urls.py
+++ core/urls.py
@@ -6,8 +6,7 @@ from .views import list_bins
 urlpatterns = [
     path("", views.home_view, name="home_view"),
     path("register/", views.register_view, name="register"),
-    path("login/", auth_views.LoginView.as_view(redirect_authenticated_user=True, template_name="core/auth/login.html", next_page="inventory_view"), name="login"),
-    path("inventory/", views.inventory_view, name="inventory_view"),
+    path("login/", auth_views.LoginView.as_view(redirect_authenticated_user=True, template_name="core/auth/login.html", next_page="home_view"), name="login"),
     path("expand_inventory/", views.expand_inventory_view, name="expand_inventory"),
     path("create_bin/", views.create_bin_view, name="create_bin"),
     path("add_items_to_bin/", views.add_items_to_bin_view, name="add_items_to_bin"),
