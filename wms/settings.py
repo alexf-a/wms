507b90d3e02003a63439b3e35103a257313388b8
alexf-a <alexfa0845@gmail.com>
2025-11-30 19:21:57 -0800

    Update STATICFILES_DIRS to conditionally include core/static based on DEBUG setting

--- wms/settings.py
+++ wms/settings.py
@@ -175,9 +175,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [
-    BASE_DIR / "core" / "static",
-]
+# Only include core/static in STATICFILES_DIRS during development.
+# In production, rely on app-based static file discovery to avoid duplication.
+STATICFILES_DIRS = [BASE_DIR / "core" / "static"] if DEBUG else []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
