d87d8e7124c787d9613c08ace6ffe467eae9f2ea
alexf-a <alexfa0845@gmail.com>
2026-02-03 12:16:44 -0800

    Enhance Item model and forms to support quantity tracking with validation and display

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
-from .models import Location, LocationSharedAccess, Unit, UnitSharedAccess
+from .models import Item, Location, LocationSharedAccess, Unit, UnitSharedAccess
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    """Admin interface for Item model."""
+
+    list_display = ("name", "user", "unit", "quantity", "quantity_unit", "formatted_quantity", "created_on")
+    list_filter = ("user", "unit", "created_on")
+    search_fields = ("name", "description")
+    readonly_fields = ("created_on", "formatted_quantity")
+
 
 # Register your models here.
 admin.site.register(Location)
